a905cd6cea9ce04f29f390136e73edeef2dca693
James Pendarvis Jr <James.PendarvisJr@us.kbr.com>
2021-03-17 15:04:50 -0400

    Adding Detials

--- Spice/Areas/Admin/Controllers/CategoryController.cs
+++ Spice/Areas/Admin/Controllers/CategoryController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Spice.Data;
+using Spice.Models;
 
 namespace Spice.Areas.Admin.Controllers
 {
@@ -26,6 +27,112 @@ namespace Spice.Areas.Admin.Controllers
 		public IActionResult Create()
 		{
 			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create(Category category)
+		{
+			if (ModelState.IsValid)
+			{
+				await _db.Category.AddAsync(category);
+				await _db.SaveChangesAsync();
+
+				return RedirectToAction(nameof(Index));
+			}
+			else
+			{
+				return View(category);
+			}
+		}
+
+		public async Task<IActionResult> Edit(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			var category = await _db.Category.FindAsync(id);
+			if (category == null)
+			{
+				return NotFound();
+			}
+
+			return View(category);
+
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(Category category)
+		{
+			if (!ModelState.IsValid) return View((category));
+			
+			_db.Update(category);
+			await _db.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
+
+		public async Task<IActionResult> Delete(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			var category = await _db.Category.FindAsync(id);
+			if (category == null)
+			{
+				return NotFound();
+			}
+
+			return View(category);
+		}
+
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			
+			var category = await _db.Category.FindAsync(id);
+			if (category == null)
+			{
+				return View();
+			}
+			_db.Category.Remove(category);
+			await _db.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
+
+		public async Task<IActionResult> Details(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			var category = await _db.Category.FindAsync(id);
+			if (category == null)
+			{
+				return NotFound();
+			}
+
+			return View(category);
+		}
+
+		[HttpPost, ActionName("Details")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DetailActionResult(int? id)
+		{
+
+			var category = await _db.Category.FindAsync(id);
+			if (category == null)
+			{
+				return View();
+			}
+
+			return RedirectToAction("Edit",id);
 		}
 	}
 }
